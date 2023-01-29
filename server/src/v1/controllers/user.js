79cf2f1c15d0c9c74ed16348c2467a504bc1faf1
Simone Westphal <s.westphal@dennree.de>
2023-01-29 16:15:38 +0100

    add user controller with register function

---
+++ server/src/v1/controllers/user.js
@@ -0,0 +1,34 @@
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+const jsonwebtoken = require('jsonwebtoken')
+const gravatar = require('gravatar')
+const config = require('config')
+
+exports.register = async (req, res) => {
+    const avatar = gravatar.url(req.body.email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+    })
+
+    const user = new User({
+        ...req.body,
+        avatar
+    })
+
+    try {
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(req.body.password, salt)
+
+        await User.create(user)
+
+        const token = jsonwebtoken.sign(
+            { id: user._id },
+            config.get('TOKEN_SECRET_KEY'),
+            { expiresIn: '24h' }
+        )
+        res.status(201).json({ user, token })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
