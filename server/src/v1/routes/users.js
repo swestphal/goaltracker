674765bae127505e1de1384e80632b30ee64e878
Simone Westphal <s.westphal@dennree.de>
2023-01-29 14:56:18 +0100

    add user post route and removed test get route

--- server/src/v1/routes/users.js
+++ server/src/v1/routes/users.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const { body } = require('express-validator')
+const validation = require('./../handlers/validation')
 
-// @route   GET api/v1/users
-// @desc    Test route
+// @route   POST api/v1/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('Users route'))
+router.post('/', [
+    body('username', 'Username is required').not().isEmpty(),
+    body('username', 'Username must have 6 or more characters').isLength({ min: 6 }),
+    body('email', 'Please enter a valid email').isEmail(),
+    body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+],
+validation.validate,
+(req, res) => res.send('Users route'))
 
 module.exports = router
