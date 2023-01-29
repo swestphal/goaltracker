42a695212f4f323a4715a7f29dea1c5c85998595
Simone Westphal <s.westphal@dennree.de>
2023-01-29 19:07:29 +0100

    wrap custom validation for unique email and username in a promise because of UnhandledPromiseRejectionWarning

--- server/src/v1/routes/users.js
+++ server/src/v1/routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const { body } = require('express-validator')
 const validation = require('./../handlers/validation')
 const userController = require('../controllers/user')
+const User = require('./../models/user')
+
 // @route   POST api/v1/users
 // @desc    Register user
 // @access  Public
@@ -10,7 +12,36 @@ router.post('/', [
     body('username', 'Username is required').not().isEmpty(),
     body('username', 'Username must have 6 or more characters').isLength({ min: 6 }),
     body('email', 'Please enter a valid email').isEmail(),
-    body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+    body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+    body('username').custom(value => {
+        // in order to avoid UnhandledPromiseRejectionWarning
+        // wrap the custom validation code in a promise
+        // code before
+        // return User.findOne({ name: value }, (err, user) => {
+        //     if (user) {
+        //         return Promise.reject('already in use');
+        //     }
+        // })
+        return new Promise((resolve, reject) => {
+            User.findOne({ username: value }, (_err, user) => {
+                if (user) {
+                    return reject(new Error('Username is already in use'))
+                }
+                resolve()
+            })
+        })
+    }),
+    body('email').custom(value => {
+        return new Promise((resolve, reject) => {
+            User.findOne({ email: value }, (_err, user) => {
+                if (user) {
+                    return reject(new Error('Email is already in use'))
+                }
+                resolve()
+            })
+        })
+    })
+
 ],
 validation.validate,
 userController.register,
