3125e316f52f57609ef3fdb162ada542867ccc6f
Simone Westphal <s.westphal@dennree.de>
2023-02-01 18:07:24 +0100

    authenticate token and return user in middleware

--- server/src/v1/middleware/auth.js
+++ server/src/v1/middleware/auth.js
@@ -1,7 +1,15 @@
 const jsonwebtoken = require('jsonwebtoken')
 const config = require('config')
+const User = require('./../models/User')
 
-module.exports = (req, res, next) => {
+/**
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ * @returns authenticated user
+ */
+exports.verifyToken = async (req, res, next) => {
     const token = req.header('x-auth-token')
 
     if (!token) {
@@ -9,7 +17,10 @@ module.exports = (req, res, next) => {
     }
     try {
         const tokenDecoded = jsonwebtoken.verify(token, config.get('TOKEN_SECRET_KEY'))
-        req.user = tokenDecoded.user
+        const userId = tokenDecoded.user.id
+        const user = await User.findById(userId).select('-password')
+        if (!user) return res.status(401).json('Unathorized')
+        req.user = user
         next()
     } catch (error) {
         res.status(401).json({ msg: 'Token not valid' })
