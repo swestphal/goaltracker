27823d86b987b381d333627de2df91823a5b99e8
Simone Westphal <s.westphal@dennree.de>
2023-02-01 18:38:32 +0100

    added post auth route

--- server/src/v1/routes/auth.js
+++ server/src/v1/routes/auth.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('./../middleware/auth')
+const { body } = require('express-validator')
+const validation = require('./../middleware/validation')
+const userController = require('../controllers/user')
 
 // @route   GET api/v1/auth
 // @desc    Test route
@@ -14,5 +17,16 @@ router.get('/', auth.verifyToken, async (req, res) => {
         res.status(500).send('Server Error')
     }
 })
+
+// @route   POST api/v1/auth
+// @desc    Authenticate user and get token
+// @access  Public
+router.post('/', [
+    body('password', 'Paword is required').exists(),
+    body('email', 'Please enter a valid email').isEmail()
+],
+validation.validate,
+userController.login,
+(req, res) => res.send('Users route'))
 
 module.exports = router
