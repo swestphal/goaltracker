36dc0d807eac17bf71057177d8ffe26b6b246694
Simone Westphal <s.westphal@dennree.de>
2023-02-05 10:07:14 +0100

    add cors package

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) // allows values to be of any type
