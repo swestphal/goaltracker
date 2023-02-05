4230a7a69ae565e9a63ccd422b51829e7164eb92
Simone Westphal <s.westphal@dennree.de>
2023-02-05 12:56:07 +0100

    refactor sidebar and display user from store

--- client/src/components/shared/Sidebar.js
+++ client/src/components/shared/Sidebar.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import { Drawer, IconButton, List, ListItemButton, Typography } from '@mui/material'
+import assets from '../../assets/index'
+import { Box } from '@mui/system'
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
 
 const Sidebar = () => {
-    return ( <>Sidebar</> );
+  const user = useSelector((state) => state.user.value)
+  const sidebarWidth = 250
+  return (<Drawer
+    container={window.document.body}
+    variant='permanent'
+    open={true}
+    sx={{
+      width:sidebarWidth,
+      height: '100%',
+      '& > div': { borderRight: 'none'}
+    }}>
+    <List
+      disablePadding
+      sx={{
+        width: sidebarWidth,
+        height: '100vH',
+        backgroundColor: assets.colors.secondary
+      }}>
+      <ListItemButton>
+        <Box sx={{
+          width:'100%',
+          display:'flex',
+          alignItems:'center',
+          justifyContent:'space-between'
+        }}>
+          <Typography variant='body2' fontWeight='700'>
+            {user.username}
+          </Typography>
+          <IconButton>
+            <LogoutOutlinedIcon/>
+          </IconButton></Box>
+      </ListItemButton>
+    </List>
+  </Drawer>
+  )
 }
  
 export default Sidebar;
