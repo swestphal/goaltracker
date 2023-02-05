190340881231d3580d11e27debb34917185abd45
Simone Westphal <s.westphal@dennree.de>
2023-02-05 12:53:41 +0100

    add redux provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
 import App from './App';
-
+import { store } from './redux/store'
+import { Provider} from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+ 
 )
