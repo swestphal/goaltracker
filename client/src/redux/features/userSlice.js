cfb22769d23888b0e64b3fc04187d9de0a896aca
Simone Westphal <s.westphal@dennree.de>
2023-02-05 12:52:12 +0100

    add redux to store user

---
+++ client/src/redux/features/userSlice.js
@@ -0,0 +1,21 @@
+import { createSlice} from '@reduxjs/toolkit'
+
+const initialState = {
+  value: {}
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    // https://stackoverflow.com/questions/60806105/
+    // error-an-immer-producer-returned-a-new-value-and-modified-its-draft-either-r
+    setUser: (state, action) => void(state.value = action.payload)
+  }
+})
+
+export const {setUser} = userSlice.actions
+
+export default userSlice.reducer
+
+
