b0652e128aca0cfea5926ccab5a3a4eb387e4174
Simone Westphal <s.westphal@dennree.de>
2023-02-05 12:55:25 +0100

    store user in store

--- client/src/components/layout/AppLayout.js
+++ client/src/components/layout/AppLayout.js
@@ -4,24 +4,27 @@ import { Outlet, useNavigate } from 'react-router-dom'
 import authUtils from '../../utils/authUtils'
 import Loading from '../shared/Loading'
 import Sidebar from '../shared/Sidebar'
+import { useDispatch } from 'react-redux'
+import { setUser } from '../../redux/features/userSlice'
 
 const AppLayout = () => {
   const navigate = useNavigate()
   const [loading, setLoading] = useState(true)
-
+  const dispatch = useDispatch()
+  
   useEffect(() => {
     const checkAuth = async () => {
       const user = await authUtils.isAuthenticated()
-      console.log("user", user)
       if (!user) {
         navigate('/login')
       } else {
         // save user
-        
+        dispatch(setUser(user))
         setLoading(false)
       }
     }
     checkAuth()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [navigate])
 
   return (
