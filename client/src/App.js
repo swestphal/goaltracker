8d9b42d4d0387aa499a9937c710fa48c627a2313
Simone Westphal <s.westphal@dennree.de>
2023-02-08 13:08:17 +0100

    change route to single board / overview

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AppLayout from './components/layout/AppLayout'
 import AuthLayout from './components/layout/AuthLayout'
 import Home from './pages/Home'
-
+import Board from './pages/Board'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 
@@ -28,10 +28,10 @@ function App() {
             <Route path='login' element={<Login/>}/>
             <Route path='signup' element={<Signup/>}/>
           </Route>
-          <Route path='/' element={<AppLayout/>}>
-            <Route index element={<Home/>}/>
-            <Route path='boards' element={<Home/>}/>
-            <Route path='boards' element={<Home/>}/>
+          <Route path='/' element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path='boards' element={<Home />} />
+            <Route path='boards/:boardId' element={<Board />} />
           </Route>
         </Routes></BrowserRouter>
     </ThemeProvider>
