dbab1fd23ffbb4b500e0c4ee69420ac759b0b33b
Simone Westphal <s.westphal@dennree.de>
2023-02-08 12:52:05 +0100

    add button to create a goal

--- client/src/components/shared/Sidebar.js
+++ client/src/components/shared/Sidebar.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Drawer, IconButton, List, ListItem, Typography } from '@mui/material'
 import assets from '../../assets/index'
 import { Box } from '@mui/system'
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
 import { useNavigate } from 'react-router-dom'
-console.log(assets)
+import boardApi from '../../api/boardApi'
+import { setBoards } from '../../redux/features/boardSlice'
+
 const Sidebar = () => {
   const user = useSelector((state) => state.user.value)
+  const boards = useSelector((state) => state.board.value)
   const navigate= useNavigate()
+  const dispatch = useDispatch()
   const sidebarWidth = 250
+
+  useEffect(()=> {
+    const getBoards = async () => {
+      try {
+        const res = await boardApi.getAll()
+        dispatch(setBoards(res))
+      } catch(err) {
+        console.log(err)
+      }
+    }
+    getBoards()
+  }, [ dispatch ])
+
+  useEffect(()=> {
+    console.log(boards)
+  }, [ boards ])
 
   const logout = ()=> {
     localStorage.removeItem('token')
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box } from '@mui/material'
 import LoadingButton from '@mui/lab/LoadingButton'
 import assets from '../assets';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import boardApi from '../api/boardApi';
+import { setBoards } from '../redux/features/boardSlice';
 
 const Home = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [ loading, setLoading ] = useState()
+
+  const createBoard = async () => {
+    setLoading(true)
+    try {
+      const res = await boardApi.create()
+      dispatch(setBoards([ res ]))
+      navigate(`/boards/${res.id}`)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return ( 
     <Box sx = {{
       height:'100%',
@@ -11,7 +32,7 @@ const Home = () => {
       alignItems: 'center',
       justifyContent: 'center'
     }}>
-      <LoadingButton variant='outlined' sx={{ borderColor:assets.palette.primary.main, color:assets.palette.primary.main }}>
+      <LoadingButton onClick={createBoard} variant='outlined' sx={{ borderColor:assets.palette.primary.main, color:assets.palette.primary.main }}>
         Create a goal
       </LoadingButton>
     </Box> );
