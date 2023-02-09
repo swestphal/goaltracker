8245564ab268c162a68a2ded63ce5c2e0b091edb
Simone Westphal <s.westphal@dennree.de>
2023-02-09 07:51:26 +0100

    add updateposition controller for board

--- server/src/v1/controllers/board.js
+++ server/src/v1/controllers/board.js
@@ -21,3 +21,18 @@ exports.getAll = async (req, res) => {
         res.status(500).json(err)
     }
 }
+
+exports.updatePosition = async (req, res) => {
+    const { boards } = req.body
+    try {
+        for (const key in boards.reverse()) {
+            const board = boards[key]
+            await Board.findByIdAndUpdate(
+                board.id,
+                { $set: { position: key } })
+        }
+        res.status(200).json('updated')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
