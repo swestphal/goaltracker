019272b98a1d4b3910f1ea67c96a092d703ce29d
Simone Westphal <s.westphal@dennree.de>
2023-02-10 13:09:15 +0100

    add emoji picker component

--- client/src/components/shared/EmojiPicker.js
+++ client/src/components/shared/EmojiPicker.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import { Box, Typography } from '@mui/material'
+import { Picker } from 'emoji-mart'
+import 'emoji-mart/css/emoji-mart.css'
 
-const EmojiPicker = () => {
+const EmojiPicker = props => {
+  const [ selectedEmoji, setSelectedEmoji ] = useState()
+  const [ isShowPicker, setIsShowPicker ]= useState()
+  
+  useEffect(()=> {
+    setSelectedEmoji(props.icon)
+  },[ props.icon ])
+  
+  const selectEmoji = (e) => {
+    const sym = e.unified.split('-')
+    let codesArray = []
+    sym.forEach(el => codesArray.push('0x' + el))
+    const emoji = String.fromCodePoint(...codesArray)
+    setIsShowPicker(false)
+    props.onChange(emoji)
+  }
+
+   
+  const showPicker = () => setIsShowPicker(!isShowPicker)
+
   return ( 
-    <p>EmojiPicker</p>
+    <Box sx={{
+      position:'relative',
+      width:'max-content',
+      zIndex:'999' }}>
+      <Typography
+        variant='h3'
+        fontWeight='700'
+        sx={{ cursor:'pointer' }}
+        onClick={showPicker}>
+        {selectedEmoji}
+      </Typography>
+      <Box sx={{
+        display:isShowPicker ? 'block':'none',
+        position:'absolute',
+        top:'100%',
+
+      }}>
+        <Picker theme='dark' onSelect={selectEmoji} showPreview={false}/>
+
+      </Box>
+    </Box>
   )
 }
  
