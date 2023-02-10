a1efe1d1cfcfc1a9824102f21511925b86ed790f
Simone Westphal <s.westphal@dennree.de>
2023-02-10 13:12:45 +0100

    add update board controller

--- server/src/v1/controllers/board.js
+++ server/src/v1/controllers/board.js
@@ -55,3 +55,43 @@ exports.updatePosition = async (req, res) => {
         res.status(500).json(err)
     }
 }
+
+exports.update = async (req, res) => {
+    const { boardId } = req.params
+    const { title, description, favourite } = req.body
+
+    try {
+        if (title === '') req.body.title = 'Untitled'
+        if (description === '') req.body.description = 'Add description here'
+        const currentBoard = await Board.findById(boardId)
+        if (!currentBoard) return res.status(404).json('Board not found')
+
+        // update new position if favourite board has changed with update
+        if (favourite !== undefined && currentBoard.favourite !== favourite) {
+            const favourites = await Board.find({
+                user: currentBoard.user,
+                favourite: true,
+                _id: { $ne: boardId }
+            }).sort('favouritePosition')
+            if (favourite) {
+                req.body.favouritePosition = favourites.length > 0 ? favourites.length : 0
+            } else {
+                for (const key in favourites) {
+                    const element = favourites[key]
+                    await Board.findByIdAndUpdate(
+                        element.id,
+                        { $set: { favouritePosition: key } }
+                    )
+                }
+            }
+        }
+
+        const board = await Board.findByIdAndUpdate(
+            boardId,
+            { $set: req.body }
+        )
+        res.status(200).json(board)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
