5275c4f8c7e4da2e8ffd22082081dd4af944c86f
Simone Westphal <s.westphal@dennree.de>
2023-02-11 16:33:40 +0100

    add routes for favourite board

--- client/src/api/boardApi.js
+++ client/src/api/boardApi.js
@@ -4,9 +4,11 @@ const boardApi = {
   getAll: () => axiosClient.get('boards'),
   create: () => axiosClient.post('boards'),
   updatePosition: (params)=> axiosClient.put('boards',params),
+  updateFavouritePosition: (params) => axiosClient.put('boards/favourites', params),
   getOne: (id) => axiosClient.get(`boards/${id}`),
   update: (id, params)=> axiosClient.put(`boards/${id}`,params),
-  getFavourites: ()=>axiosClient.get('boards/favourites')
+  getFavourites: ()=>axiosClient.get('boards/favourites'),
+
 }
 
 export default boardApi
