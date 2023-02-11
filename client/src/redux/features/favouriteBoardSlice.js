5713816dcfa9e373084fa6802d202d1fee963109
Simone Westphal <s.westphal@dennree.de>
2023-02-11 16:32:10 +0100

    rename reducer

--- client/src/redux/features/favouriteBoardSlice.js
+++ client/src/redux/features/favouriteBoardSlice.js
@@ -4,18 +4,18 @@ const initialState = {
   value: []
 }
 
-export const favouriteSlice = createSlice({
-  name: 'favourites',
+export const favouriteBoardSlice = createSlice({
+  name: 'favouriteBoard',
   initialState,
   reducers: {
     // https://stackoverflow.com/questions/60806105/
     // error-an-immer-producer-returned-a-new-value-and-modified-its-draft-either-r
-    setFavouritesList: (state, action) => void(state.value = action.payload)
+    setFavouriteBoards: (state, action) => void(state.value = action.payload)
   }
 })
 
-export const { setFavouritesList } = favouriteSlice.actions
+export const { setFavouriteBoards } = favouriteBoardSlice.actions
 
-export default favouriteSlice.reducer
+export default favouriteBoardSlice.reducer
 
 
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './features/userSlice'
 import boardReducer from './features/boardSlice'
-import favoriteReducer from './features/favouriteSlice'
+import favouriteBoardReducer from './features/favouriteBoardSlice'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     board: boardReducer,
-    favorites: favoriteReducer
+    favouriteBoard: favouriteBoardReducer
   }
 })
