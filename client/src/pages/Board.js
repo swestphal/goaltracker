3bbf02193d6cc059ac44d8a6d8b603651a40523f
Simone Westphal <s.westphal@dennree.de>
2023-02-11 10:11:11 +0100

    add update title

--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -1,20 +1,27 @@
-import { Button, Divider, IconButton, TextField, Typography } from '@mui/material';
+import { Button, Divider, IconButton, TextField, Typography } from '@mui/material'
 import StarOutlinedIcon  from '@mui/icons-material/StarOutlined'
 import StarBorderOutlinedIcon  from '@mui/icons-material/StarBorderOutlined'
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutlined'
-import { Box } from '@mui/system';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import boardApi from '../api/boardApi';
-import EmojiPicker from '../components/shared/EmojiPicker';
+import { Box } from '@mui/system'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import boardApi from '../api/boardApi'
+import EmojiPicker from '../components/shared/EmojiPicker'
+import { useDispatch, useSelector } from 'react-redux';
+import { setBoards } from '../redux/features/boardSlice'
+import { createSerializableStateInvariantMiddleware } from '@reduxjs/toolkit'
 
 const Board = () => {
+  const dispatch = useDispatch()
   const { boardId } = useParams()
   const [ title, setTitle ] = useState('')
   const [ description, setDescription ] = useState('')
   const [ sections, setSections ] = useState([])
   const [ isFavourite, setIsFavourite ] = useState(false)
   const [ icon, setIcon ] = useState('')
+  const [ timer, setTimer ] = useState(null)
+
+  const boards = useSelector((state)=> state.board.value)
 
   useEffect(()=> {
     const getBoard = async()=> {
@@ -35,9 +42,9 @@ const Board = () => {
 
   const onIconChange = async (newIcon) => {
     setIcon(newIcon)
-    // let temp = [ ...boards ]
-    // const index = temp.findIndex(e => e.id === boardId)
-    // temp[index] = { ...temp[index], icon: newIcon }
+    let temp = [ ...boards ]
+    const index = temp.findIndex(e => e.id === boardId)
+    temp[index] = { ...temp[index], icon: newIcon }
 
     // if (isFavourite) {
     //   let tempFavourite = [ ...favouriteList ]
@@ -46,15 +53,34 @@ const Board = () => {
     //   dispatch(setFavouriteList(tempFavourite))
     // }
 
-    // setIcon(newIcon)
-    // dispatch(setBoards(temp))
-    // try {
-    //   await boardApi.update(boardId, { icon: newIcon })
-    // } catch (err) {
-    //   alert(err)
-    // }
+    setIcon(newIcon)
+    dispatch(setBoards(temp))
+    try {
+      await boardApi.update(boardId, { icon: newIcon })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
+  const updateTitle = async(e)=> {
+    clearTimeout(timer)
+    const newTitle= e.target.value
+    setTitle(newTitle)
+    
+    const newTimer = setTimeout(async ()=> {
+      let temp = [ ...boards ]
+      const index = temp.findIndex(e => e.id === boardId)
+      temp[index] = { ...temp[index], title: newTitle }
+      try {
+        await boardApi.update(boardId, { title: newTitle })
+        dispatch(setBoards(temp))
+      } catch (err) {
+        console.log(err)
+      }
+    },2000)
+
+    setTimer(newTimer)
+  }
   
   return ( 
     <>
@@ -78,6 +104,7 @@ const Board = () => {
           <EmojiPicker icon={icon} onChange={onIconChange}/>
           <TextField 
             value={title}
+            onChange={updateTitle}
             placeholder="Untitled"
             variant="outlined"
             fullWidth
