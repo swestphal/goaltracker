1b44cade4f7705539cf63d739a35b5574371375b
Simone Westphal <s.westphal@dennree.de>
2023-02-11 18:11:52 +0100

    add boardSection controller to create, update and delete section of board

---
+++ server/src/v1/controllers/boardSection.js
@@ -0,0 +1,39 @@
+const Board = require('./../models/Board')
+const Section = require('./../models/BoardSection')
+const Task = require('./../models/BoardTask')
+
+exports.create = async (req, res) => {
+    const { boardId } = req.params
+    try {
+        const section = await Section.create({ board: boardId })
+        section._do.tasks = []
+        res.status(200).json(section)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+exports.update = async (req, res) => {
+    const { sectionId } = req.params
+    try {
+        const section = await Section.findByIdAndUpdate(
+            sectionId,
+            { $set: req.body }
+        )
+        section._doc.tasks = []
+        res.status(200).json(section)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+exports.delete = async (req, res) => {
+    const { sectionId } = req.params
+    try {
+        await Task.deleteMany({ section: sectionId })
+        await Section.deleteOne({ _id: sectionId })
+        res.status(200).json('deleted')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
