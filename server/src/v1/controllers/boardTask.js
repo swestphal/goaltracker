2215b853b744a35863bffb99ac8284f0f820e03b
Simone Westphal <simone.westphal.me@gmail.com>
2023-02-26 17:46:29 +0100

    add task controller

---
+++ server/src/v1/controllers/boardTask.js
@@ -0,0 +1,91 @@
+const Section = require('./../models/BoardSection')
+const Task = require('./../models/BoardTask')
+
+exports.create = async (req, res) => {
+    const { sectionId } = req.params
+    try {
+        const section = await Section.findById(sectionId)
+        const tasksCount = await Task.find({ section: sectionId }).count()
+        const task = await Task.create({
+            section: sectionId,
+            position: tasksCount > 0 ? tasksCount : 0
+
+        })
+        task._doc.section = section
+        res.status(200).json(task)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+exports.update = async (req, res) => {
+    const { taskId } = req.params
+    try {
+        const task = await Task.findByIdAndUpdate(
+            taskId,
+            { $set: req.body }
+        )
+        res.status(200).json(task)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+exports.delete = async (req, res) => {
+    const { taskId } = req.params
+    try {
+        const currentTask = await Task.findById(taskId)
+        await Section.deleteOne({ _id: taskId })
+        const tasks = await Task.find({ section: currentTask.section }).sort('position')
+        for (const key in tasks) {
+            await Task.findByIdAndUpdate(
+                tasks[key].id,
+                { $set: { position: key } }
+            )
+        }
+        res.status(200).json('deleted')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+exports.updatePosition = async (req, res) => {
+    const {
+        resourceList,
+        destinationList,
+        resourceSectionId,
+        destinationSectionId
+    } = req.body
+
+    const resourceListReverse = resourceList.reverse()
+    const destinationListReverse = destinationList.reverse()
+    try {
+        if (resourceSectionId !== destinationSectionId) {
+            for (const key in resourceListReverse) {
+                await Task.findByIdAndUpdate(
+                    resourceListReverse[key].id,
+                    {
+                        $set: {
+                            section: resourceSectionId,
+                            position: key
+                        }
+                    }
+                )
+            }
+        }
+        for (const key in destinationListReverse) {
+            await Task.findByIdAndUpdate(
+                destinationListReverse[key].id,
+                {
+                    $set: {
+                        section: destinationSectionId,
+                        position: key
+                    }
+                }
+            )
+        }
+        res.status(200).json('updated')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
